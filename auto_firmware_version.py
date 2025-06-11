8441e2be2a801583b80ba97a3d53573b620252fd
Tibor Krizsák <t.krizsak@topdesk.com>
2025-06-11 22:24:00 +0200

    chore: revising build version one last time

--- auto_firmware_version.py
+++ auto_firmware_version.py
@@ -3,12 +3,12 @@ import subprocess
 Import("env")
 
 def get_firmware_specifier_build_flag():    
-    ret = subprocess.run(["git", "tag"], stdout=subprocess.PIPE, text=True)
+    ret_tag = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], stdout=subprocess.PIPE, text=True)
     ret_commit = subprocess.run(["git", "describe", "--always"], stdout=subprocess.PIPE, text=True)
-    build_version = ret.stdout.strip()
+    tag = ret_tag.stdout.strip()
     commit = ret_commit.stdout.strip()
-    build_flag = "-D AUTO_VERSION=\\\"" + build_version + "-" + commit + "\\\""
-    print ("Firmware Revision: " + build_version)
+    build_flag = "-D AUTO_VERSION=\\\"" + tag + "-" + commit + "\\\""
+    print ("Firmware Revision: " + tag)
     return (build_flag)
 
 env.Append(
